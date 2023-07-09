4b07bb79bbe11fa929518793f174658815c7bc74
Jotaro Sugiyama <sugi.jotaro@gmail.com>
2023-07-09 14:17:32 +0900

    Initial Commit

---
+++ LocationDemoApp/LocationDemoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocationDemoAppApp.swift
+//  LocationDemoApp
+//
+//  Created by JotaroSugiyama on 2023/07/09.
+//
+
+import SwiftUI
+
+@main
+struct LocationDemoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
